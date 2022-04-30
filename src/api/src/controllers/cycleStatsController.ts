042f98d14e1bf57f21ec7ab0d18d7ec3c84f911e
Christian Tinnefeld <193486+tinnefeld@users.noreply.github.com>
2022-04-30 07:50:47 -0700

    CS-005: add oauth support

--- src/api/src/controllers/cycleStatsController.ts
+++ src/api/src/controllers/cycleStatsController.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import { StatisticsService } from "../services/StatisticsService.js";
+import { StravaImportService } from "../services/StravaImportService.js";
 
 export async function getStatistics(req: express.Request, res: express.Response): Promise<void> {
   const statisticsService = new StatisticsService();
@@ -11,4 +12,20 @@ export async function getSummary(req: express.Request, res: express.Response): P
   const statisticsService = new StatisticsService();
   const result = await statisticsService.createSummary();
   res.json(result);
+}
+
+export async function postToken(req: express.Request, res: express.Response): Promise<void> {
+  if (!req.query.code) {
+    res.status(400).send("Missing authorization code");
+  }
+  const stravaService = new StravaImportService();
+  const result = await stravaService.getOAuthToken(req.query.code as string);
+  if (result.status === 200 || result.status === 201) {
+    const data: any = await result.json();
+    res.status(201).json({
+      bearerToken: data.access_token
+    });
+  } else {
+    res.status(result.status).send(result.statusText);
+  }
 }
--- src/api/src/services/StravaImportService.ts
+++ src/api/src/services/StravaImportService.ts
@@ -1,13 +1,18 @@
 import { promises as fsPromises } from "fs";
+import fetch, { Response as ResponseFetch } from "node-fetch";
 import { definitions } from "../models/IStrava";
 
 type IStravaSummaryActivity = definitions["SummaryActivity"];
 type IStravaDetailedAthlete = definitions["DetailedAthlete"];
 type IStravaActivityStats = definitions["ActivityStats"];
 
+const STRAVA_OAUTH_URL = "https://www.strava.com/oauth/token";
+
 const LOCAL_DATA_PATH = "./test/data/";
 
 export class StravaImportService {
+  private readonly CLIENT_ID = process.env.CLIENT_ID;
+  private readonly CLIENT_SECRET = process.env.CLIENT_SECRET;
   private readonly NODE_ENV = process.env.NODE_ENV;
 
   public async getActivities(): Promise<IStravaSummaryActivity[]> {
@@ -32,6 +37,13 @@ export class StravaImportService {
       result = await this.loadLocalData("athlete_stats_basic.json");
     }
     return result;
+  }
+
+  public async getOAuthToken(code: string): Promise<ResponseFetch> {
+    const grantType = "authorization_code";
+    const response = await fetch(`${STRAVA_OAUTH_URL}?client_id=${this.CLIENT_ID}&client_secret=${this.CLIENT_SECRET}` +
+                                 `&code=${code}&grant_type=${grantType}`, { method: "post" });
+    return response;
   }
 
   private async loadLocalData(fileName: string): Promise<any> {
